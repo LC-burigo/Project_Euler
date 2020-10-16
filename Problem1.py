313080ab1b5e4a7e2cf4775c80f55858c281db76
LC-burigo <burigo-jm@hotmail.com>
2020-10-16 02:48:25 -0300

    Problem 1 and the virtual env

---
+++ Problem1.py
@@ -0,0 +1,13 @@
+def multiples_of_3_and_5(number):
+    List_numbers = []
+    Sum_numbers = 0
+    for i in range(number):
+        if i % 3 == 0 or i % 5 == 0:
+            List_numbers.append(i)
+            Sum_numbers += i
+
+    print(List_numbers)
+    print(Sum_numbers)
+
+
+multiples_of_3_and_5(1000)
