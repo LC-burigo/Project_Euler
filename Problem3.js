fee93d2b73d824715fa137b10068002fcb8d016d
LC-burigo <burigo-jm@hotmail.com>
2020-10-16 14:05:55 -0300

    problem 3

---
+++ Problem3.js
@@ -0,0 +1,10 @@
+function prime_factor(number) {
+    if (number > 1){
+
+    }
+    else{
+    console.log('O número escolhido tem que ser maior do que 1 e inteiro')
+    }
+}
+
+prime_factor(1)
---
+++ Problem3.py
@@ -0,0 +1,26 @@
+def prime_factor(number):
+    if number > 1:
+        factors = []
+        prime_list = []
+        i = 2
+        count = 0
+        while i <= number:
+            if number % i == 0:
+                factors.append(i)
+                for e in range(1, i + 1):
+                    if i % e == 0:
+                        count += 1
+                if count == 2:
+                    prime_list.append(i)
+
+            count = 0
+            i += 1
+    else:
+        print(number, "is not a prime number")
+
+    print(factors)
+    print(prime_list)
+    print(max(prime_list))
+
+
+prime_factor(13195)
