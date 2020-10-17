12e1f787144157c7609d5ac506dc6b727cd8668b
LC-burigo <burigo-jm@hotmail.com>
2020-10-17 04:15:55 -0300

    problem 7

--- Problem7.py
+++ Problem7.py
@@ -16,4 +16,16 @@ def Position_of_a_prime(number):
 
 Position_of_a_prime(10001)
 
+# fastest way
+def n_th_prime(n):
+    b = [2]
+    while len(b) < n:
+        for num in range(3, n * 11, 2):
+            if all(num % i != 0 for i in range(2, int(num ** 0.5) + 1)):
+                b.append(num)
+
+    print(list(sorted(b))[n - 1])
+
+
+n_th_prime(10001)
 
