cc9000ad174a3708e3d40e6e5df5c48a2b054ea3
LC-burigo <burigo-jm@hotmail.com>
2020-10-18 12:54:03 -0300

    problem 9

---
+++ Problem9.py
@@ -0,0 +1,6 @@
+for c in range(999):
+    for b in range(c):
+        for a in range(b):
+            if a + b + c == 1000:
+                if (a**2) + (b**2) == c**2:
+                    print(a*b*c)
